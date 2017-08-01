68a0900c90c06327e070af7b36b24192ef430954
Sean Nam <namsangwoo1@gmail.com>
2017-08-01 11:40:47 -0700

    Created Task model and storyboard files

---
+++ RealTasks/AddTaskViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddTaskViewController.swift
+//  RealTasks
+//
+//  Created by Sean Nam on 8/1/17.
+//  Copyright © 2017 Sean Nam. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        print("Cancel button pressed")
+    }
+    
+    @IBAction func onSaveButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        print("Save button pressed")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RealTasks/Task.swift
@@ -0,0 +1,19 @@
+//
+//  Task.swift
+//  RealTasks
+//
+//  Created by Sean Nam on 8/1/17.
+//  Copyright © 2017 Sean Nam. All rights reserved.
+//
+
+import UIKit
+
+class Task: NSObject {
+    
+    var taskName = ""
+    var taskDetails = ""
+    var priority = 0
+    var dueDate = ""
+    var createdDate = ""
+
+}
