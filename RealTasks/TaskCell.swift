0c336ef64c5ee52f2688dafd356dcebb91e333a4
Sean Nam <namsangwoo1@gmail.com>
2017-08-01 14:17:21 -0700

    Created gitignore file

---
+++ RealTasks/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  RealTasks
+//
+//  Created by Sean Nam on 8/1/17.
+//  Copyright © 2017 Sean Nam. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
